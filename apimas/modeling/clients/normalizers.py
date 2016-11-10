e1634b73c3162563bfce6ecd0d1937c4565e5f2a
Thodoris Sotiropoulos <theosot@grnet.gr>
2016-11-10 13:59:08 +0200

    Add normalizers for Date and Datetime

--- apimas/modeling/clients/normalizers.py
+++ apimas/modeling/clients/normalizers.py
@@ -1,3 +1,4 @@
+from datetime import datetime, date
 from requests.compat import urljoin
 
 
@@ -13,3 +14,29 @@ class RefNormalizer(object):
 
     def __call__(self, value):
         return urljoin(self.ref_endpoint, value) + '/'
+
+
+class DateTimeNormalizer(object):
+    """
+    Normalize a datetime object to a string value based on the given format.
+
+    If value is string, then it is checked if it follows the given format.
+    """
+    DEFAULT_FORMAT = '%Y-%m-%dT%H:%M:%S'
+
+    def __init__(self, date_format=None):
+        self.date_format = date_format or self.DEFAULT_FORMAT
+
+    def __call__(self, value):
+        if isinstance(value, date) and not isinstance(value, datetime):
+            value = datetime.combine(value, datetime.min.time())
+            return value.strftime(self.date_format)
+        elif isinstance(value, datetime):
+            return value.strftime(self.date_format)
+        elif isinstance(value, str):
+            datetime.strptime(value, self.date_format)
+        return value
+
+
+class DateNormalizer(DateTimeNormalizer):
+    DEFAULT_FORMAT = '%Y-%m-%d'
