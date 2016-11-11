8f8b7fafd596bfcc28a2e12d82c6bab3062d3345
Thodoris Sotiropoulos <theosot@grnet.gr>
2016-11-11 12:43:50 +0200

    Correct setup entrypoint

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=[],
     entry_points={
         'console_scripts': {
-            'apimas=apimas.cli.apimas:main'
+            'apimas=apimas.modeling.cli.cli:main'
         }
     }
 )
