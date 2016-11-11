f43319a91fe4a43396a00f18992fe1fab04dba7a
Georgios D. Tsoukalas <gtsouk@grnet.gr>
2016-11-11 16:27:06 +0200

    Extend tabmatch to use doc_match_levels() via multimatch()

--- apimas/modeling/permissions/tabmatch.py
+++ apimas/modeling/permissions/tabmatch.py
@@ -1,27 +1,38 @@
 from collections import namedtuple
+from apimas.modeling.core.documents import (
+    doc_set, doc_match, doc_iter, ANY, Prefix, Regex)
 
 
 class Tabmatch(object):
-    def __init__(self, column_names):
+    def __init__(self, column_names, rules=()):
         self.column_names = tuple(column_names)
         self.Row = namedtuple('TabmatchRow', self.column_names)
-        self.rows = set()
+        self.rules_set = set(rules)
+        self.rules_doc = {}
+        for rule in rules:
+            doc_set(self.rules_doc, rule[:-1], rule[-1])
+
+        self.name_levels = {
+            name: x
+            for x, name in enumerate(self.column_names)
+        }
 
     def _check_row_type(self, row):
         if not isinstance(row, self.Row):
             m = "rows must be of type Tabmatch.Row, not {row!r}"
-            m = m.format(row)
+            m = m.format(row=row)
             raise TypeError(m)
 
     def update(self, rows):
         for row in rows:
             self._check_row_type(row)
-            self.rows.add(row)
+            self.rules_set.add(row)
+            doc_set(self.rules_doc, row[:-1], row[-1])
 
     def match(self, row, expand):
         self._check_row_type(row)
         results = set()
-        for tab_row in self.rows:
+        for tab_row in self.rules_set:
             item = {}
             for name in self.column_names:
                 tab_val = getattr(tab_row, name)
@@ -42,3 +53,40 @@ class Tabmatch(object):
                 results.add(val)
 
         return results
+
+    def multimatch(self, pattern_sets, expand):
+        expand_levels = {self.name_levels[name] for name in expand}
+        depth = len(self.column_names)
+        matches = doc_match_levels(self.rules_doc, pattern_sets,
+                                   expand_levels)
+        for path in matches:
+            if len(path) == depth:
+                yield self.Row(*path)
+
+
+def test():
+    tb = Tabmatch(['one', 'two', 'three'])
+    tb.update([tb.Row(one='hella', two='two', three='hooves')])
+    tb.update([tb.Row(one='hellish', two='taint', three='saw')])
+    tb.update([tb.Row(one='hello', two='there', three='friend')])
+    tb.update([tb.Row(one=Prefix('foo'), two=ANY, three='blam')])
+    pattern_sets = [
+        ['alpha', Prefix('hel'), 'one'],
+        [Regex('th.r[ae]'), 'bom', Prefix('t')],
+    ]
+    print "RULES"
+    print tb.rules_doc
+    print "PATTERNS"
+    print pattern_sets
+    expand = []
+    print "EXPAND", expand
+    print list(tb.multimatch(pattern_sets, expand=[]))
+    expand = ['one', 'two', 'three']
+    print "EXPAND", expand
+    expand_levels = {tb.name_levels[name] for name in expand}
+    print list(tb.multimatch(pattern_sets, expand=expand))
+
+
+if __name__ == '__main__':
+    test()
+
