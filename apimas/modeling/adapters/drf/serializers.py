360af328e132b2ce1823e578c71698c1ffaeaf05
Thodoris Sotiropoulos <theosot@grnet.gr>
2016-11-17 18:46:49 +0200

    Patch method get_fields() of drf serializer class
    This commit fixes the issue when you set the keyword argument `source`
    and your have not set the serializer fields explicitly. Then the
    construction of field failed because there is not actually any model
    field with that name.
    
    However, you are still able to imply the name of model field by
    providing the `source` keyword argument at `Meta.extra_kwargs`.
    
    For more information, see:
    https://github.com/tomchristie/django-rest-framework/pull/4688

--- apimas/modeling/adapters/drf/serializers.py
+++ apimas/modeling/adapters/drf/serializers.py
@@ -1,4 +1,8 @@
+import copy
+from collections import OrderedDict
 from rest_framework import serializers
+from rest_framework.settings import api_settings
+from rest_framework.utils import model_meta
 from apimas.modeling.core.documents import doc_to_ns, ANY
 from apimas.modeling.adapters.drf import utils
 
@@ -37,6 +41,77 @@ class ApimasSerializer(serializers.HyperlinkedModelSerializer):
             return
         return self.set_field_property(
             segments[1:], fields.get(segments[0], {}).fields, property_key)
+
+    def get_fields(self):
+        """
+        Return the dict of field names -> field instances that should be
+        used for `self.fields` when instantiating the serializer.
+        """
+        if self.url_field_name is None:
+            self.url_field_name = api_settings.URL_FIELD_NAME
+
+        assert hasattr(self, 'Meta'), (
+            'Class {serializer_class} missing "Meta" attribute'.format(
+                serializer_class=self.__class__.__name__
+            )
+        )
+        assert hasattr(self.Meta, 'model'), (
+            'Class {serializer_class} missing "Meta.model" attribute'.format(
+                serializer_class=self.__class__.__name__
+            )
+        )
+        if model_meta.is_abstract_model(self.Meta.model):
+            raise ValueError(
+                'Cannot use ModelSerializer with Abstract Models.'
+            )
+
+        declared_fields = copy.deepcopy(self._declared_fields)
+        model = getattr(self.Meta, 'model')
+        depth = getattr(self.Meta, 'depth', 0)
+
+        if depth is not None:
+            assert depth >= 0, "'depth' may not be negative."
+            assert depth <= 10, "'depth' may not be greater than 10."
+
+        # Retrieve metadata about fields & relationships on the model class.
+        info = model_meta.get_field_info(model)
+        field_names = self.get_field_names(declared_fields, info)
+
+        # Determine any extra field arguments and hidden fields that
+        # should be included
+        extra_kwargs = self.get_extra_kwargs()
+        extra_kwargs, hidden_fields = self.get_uniqueness_extra_kwargs(
+            field_names, declared_fields, extra_kwargs
+        )
+
+        # Determine the fields that should be included on the serializer.
+        fields = OrderedDict()
+
+        for field_name in field_names:
+            # If the field is explicitly declared on the class then use that.
+            if field_name in declared_fields:
+                fields[field_name] = declared_fields[field_name]
+                continue
+
+            extra_field_kwargs = extra_kwargs.get(field_name, {})
+            source = extra_field_kwargs.get('source') or field_name
+            # Determine the serializer field class and keyword arguments.
+            field_class, field_kwargs = self.build_field(
+                source, info, model, depth
+            )
+
+            # Include any kwargs defined in `Meta.extra_kwargs`
+            field_kwargs = self.include_extra_kwargs(
+                field_kwargs, extra_field_kwargs
+            )
+
+            # Create the serializer field.
+            fields[field_name] = field_class(**field_kwargs)
+
+        # Add in any hidden fields.
+        fields.update(hidden_fields)
+
+        return fields
 
 
 def generate(model, config, is_collection=True):
