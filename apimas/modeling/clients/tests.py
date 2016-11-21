ff4d6863e07530c1c10544f540655bce8a6097e7
Thodoris Sotiropoulos <theosot@grnet.gr>
2016-11-21 11:15:47 +0200

    Add unit tests for 'ApimasClient' methods

--- apimas/modeling/clients/tests.py
+++ apimas/modeling/clients/tests.py
@@ -8,16 +8,18 @@ from apimas.modeling.core.exceptions import ApimasClientException
 
 
 class TestClients(unittest.TestCase):
+    def setUp(self):
+        self.client = ApimasClient('http://endpoint/', {})
+
     def test_actions(self):
-        client = ApimasClient('http://endpoint/', {})
         actions = {
-            'post': [(client.create, ())],
-            'put': [(client.update, 1)],
-            'patch': [(client.partial_update, 1)],
-            'get': [(client.retrieve, 1), (client.list, ())],
-            'delete': [(client.delete, 1)],
-            'head': [(client.head, ())],
-            'options': [(client.options, ())]
+            'post': [(self.client.create, ())],
+            'put': [(self.client.update, 1)],
+            'patch': [(self.client.partial_update, 1)],
+            'get': [(self.client.retrieve, 1), (self.client.list, ())],
+            'delete': [(self.client.delete, 1)],
+            'head': [(self.client.head, ())],
+            'options': [(self.client.options, ())]
         }
         mock_response_ex = mock.MagicMock()
         mock_response_ex.raise_for_status.side_effect = HTTPError()
@@ -41,16 +43,58 @@ class TestClients(unittest.TestCase):
                 'required': True
             }
         }
-        client = ApimasClient('http://endpoint/', {})
-        client.validation_schema = validation_schema
+        self.client = ApimasClient('http://endpoint/', {})
+        self.client.validation_schema = validation_schema
         data = {'field1': 1}
-        self.assertRaises(ApimasClientException, client.partial_validate, data)
+        self.assertRaises(ApimasClientException, self.client.partial_validate,
+                          data)
         try:
-            client.partial_validate(raise_exception=False, data=data)
-            client.partial_validate(
+            self.client.partial_validate(raise_exception=False, data=data)
+            self.client.partial_validate(
                 raise_exception=True, data={'field1': 'foo'})
         except ApimasClientException:
             self.fail()
+
+    def test_extract_files(self):
+        mock_file = mock.MagicMock(spec=file)
+        data = {'field1': {'field2': mock_file}}
+        self.assertRaises(ApimasClientException, self.client.extract_files,
+                          data)
+        data['field1']['field2'] = 'a value'
+        data['field3'] = mock_file
+        self.assertRaises(ApimasClientException, self.client.extract_files,
+                          data)
+
+        data = {'field1': 'a value', 'field2': mock_file,
+                'field3': 'another value'}
+        files = self.client.extract_files(data)
+        self.assertEqual(files, {'field2': mock_file})
+        self.assertEqual(data, {'field1': 'a value',
+                                'field3': 'another value'})
+
+    def test_extract_file_data(self):
+        mock_client = mock.Mock(
+            extract_write_data=ApimasClient.__dict__['extract_write_data'])
+        mock_client.extract_write_data(mock_client, {}, True)
+        self.assertEqual(mock_client.validate.call_count, 1)
+        self.assertEqual(mock_client.partial_validate.call_count, 0)
+
+        mock_client.extract_write_data(mock_client, {}, True,
+                                       partial=True)
+
+        self.assertEqual(mock_client.validate.call_count, 1)
+        self.assertEqual(mock_client.partial_validate.call_count, 1)
+
+        mock_client.extract_files.return_value = {}
+        data = mock_client.extract_write_data(mock_client, {}, True)
+        self.assertEqual(len(data), 1)
+        self.assertIsNotNone(data['json'])
+
+        mock_client.extract_files.return_value = 'a value'
+        data = mock_client.extract_write_data(mock_client, {}, True)
+        self.assertEqual(len(data), 2)
+        self.assertIsNotNone(data['files'])
+        self.assertIsNotNone(data['data'])
 
 
 class TestAuth(unittest.TestCase):
