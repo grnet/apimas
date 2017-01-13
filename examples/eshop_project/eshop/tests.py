99f427c0168397b412e0290edab55fd5e6a48d41
Thodoris Sotiropoulos <theosot@grnet.gr>
2017-01-13 18:31:10 +0200

    eshop: Add tests using 'ApimasTestCase'

--- examples/eshop_project/eshop/tests.py
+++ examples/eshop_project/eshop/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.contrib.auth.models import Group
+from apimas.modeling.adapters.drf.testing import (
+    ApimasTestCase, set_apimas_context)
+from eshop.api_spec import API_SPEC
+from eshop.models import UserProfile
 
-# Create your tests here.
+
+@set_apimas_context(__name__, API_SPEC)
+class TestApplication(ApimasTestCase):
+    def authenticate(self):
+        user = UserProfile.objects.create(username='fdsfa', first_name='fdadaf',
+                                          last_name='fdsfd', password='fdafda',
+                                          email='test@example.com')
+        group = Group.objects.create(name='group name')
+        user.groups.add(group)
+        self.client.force_authenticate(user=user)
+
+    def authenticate_orders(self, collection):
+        self.authenticate()
+
+    def authenticate_carts(self, collection):
+        self.authenticate()
