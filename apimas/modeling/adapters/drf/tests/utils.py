361636e573fada40f73a4ed9932b155a27a98df2
Thodoris Sotiropoulos <theosot@grnet.gr>
2017-01-31 14:34:31 +0200

    Make spec generator class-based

--- apimas/modeling/adapters/drf/tests/utils.py
+++ apimas/modeling/adapters/drf/tests/utils.py
@@ -27,62 +27,65 @@ ACTIONS = {
 PROPERTIES = {'.required': {}}
 
 
-def generate_ref_field(model_field):
-    ref = model_field.related_model.__name__ + '_collection'
-    many = model_field.many_to_many or model_field.one_to_many
-    return {'.ref': {'to': ref, 'many': many}}
+class SpecGenerator(object):
 
+    def __init__(self, endpoint=None, permissions=None):
+        self.endpoint = endpoint or utils.generate_random_string(
+            max_length=10)
+        self.permissions = permissions or [('*',) * 6]
 
-def generate_structure(model_field):
-    if model_field.one_to_one or model_field.many_to_one:
-        predicate_type = '.struct'
-    else:
-        predicate_type = '.structarray'
-    ref_model = model_field.related_model
-    model_fields = filter((lambda x: isinstance(x, models.Field)),
-                          ref_model._meta.get_fields())
-    field_schema = generate_field_schema(model_fields, iscollection=False)
-    return {predicate_type: field_schema, '.readonly': {}}
+    def generate(self, test_models):
+        spec = {self.endpoint: {}}
+        for model in test_models:
+            model_cls = import_object(model)
+            collection_name = model_cls.__name__ + '_collection'
+            model_fields = filter((lambda x: isinstance(x, models.Field)),
+                                  model_cls._meta.get_fields())
+            field_schema = self.generate_field_schema(model_fields)
+            collection_spec = copy.deepcopy(COLLECTION_TEMPLATE)
+            collection_spec['.drf_collection']['model'] = model
+            collection_spec['*'] = field_schema
+            collection_spec['actions'] = ACTIONS
+            spec[self.endpoint][collection_name] = collection_spec
+            spec[self.endpoint].update(
+                {'.endpoint': {'permissions': self.permissions}})
+        return spec
 
+    def generate_ref_field(self, model_field):
+        ref = model_field.related_model.__name__ + '_collection'
+        many = model_field.many_to_many or model_field.one_to_many
+        return {'.ref': {'to': self.endpoint + '/' + ref, 'many': many}}
 
-REL_FIELDS_CONSTRUCTORS = [
-    generate_ref_field,
-    generate_structure,
-]
-
-
-def generate_field_schema(model_fields, iscollection=True):
-    field_schema = {}
-    for model_field in model_fields:
-        if model_field.related_model is None:
-            predicate_type = '.' + utils.FIELD_TYPE_MAPPING[type(model_field)]
-            field_node = {'.drf_field': {}, predicate_type: {}}
+    def generate_structure(self, model_field):
+        if model_field.one_to_one or model_field.many_to_one:
+            predicate_type = '.struct'
         else:
-            spec_field = random.choice(REL_FIELDS_CONSTRUCTORS)(model_field)
-            field_node = dict({'.drf_field': {}}, **spec_field)
-        if '.struct' not in field_node and '.structarray' not in field_node:
-            field_schema[model_field.name] = dict(field_node, **PROPERTIES)
-    identity_field = utils.generate_random_string(max_length=4)
-    if iscollection:
-        field_schema[identity_field] = {'.drf_field': {}, '.identity': {}}
-    return field_schema
-
-
-def generate_random_spec(test_models):
-    default_permissions = [('*',) * 6]
-    api_name = utils.generate_random_string(max_length=10)
-    spec = {api_name: {}}
-    for model in test_models:
-        model_cls = import_object(model)
-        collection_name = model_cls.__name__ + '_collection'
+            predicate_type = '.structarray'
+        ref_model = model_field.related_model
         model_fields = filter((lambda x: isinstance(x, models.Field)),
-                              model_cls._meta.get_fields())
-        field_schema = generate_field_schema(model_fields)
-        collection_spec = copy.deepcopy(COLLECTION_TEMPLATE)
-        collection_spec['.drf_collection']['model'] = model
-        collection_spec['*'] = field_schema
-        collection_spec['actions'] = ACTIONS
-        spec[api_name][collection_name] = collection_spec
-        spec[api_name].update(
-            {'.endpoint': {'permissions': default_permissions}})
-    return spec
+                              ref_model._meta.get_fields())
+        field_schema = self.generate_field_schema(model_fields,
+                                                  iscollection=False)
+        return {predicate_type: field_schema, '.readonly': {}}
+
+    def generate_field_schema(self, model_fields, iscollection=True):
+        rel_field_constructors = [
+            self.generate_ref_field,
+            self.generate_structure,
+        ]
+        field_schema = {}
+        for model_field in model_fields:
+            if model_field.related_model is None:
+                predicate_type = '.' + utils.FIELD_TYPE_MAPPING[type(
+                    model_field)]
+                field_node = {'.drf_field': {}, predicate_type: {}}
+            else:
+                spec_field = random.choice(rel_field_constructors)(
+                    model_field)
+                field_node = dict({'.drf_field': {}}, **spec_field)
+            if '.struct' not in field_node and '.structarray' not in field_node:
+                field_schema[model_field.name] = dict(field_node, **PROPERTIES)
+        identity_field = utils.generate_random_string(max_length=4)
+        if iscollection:
+            field_schema[identity_field] = {'.drf_field': {}, '.identity': {}}
+        return field_schema
