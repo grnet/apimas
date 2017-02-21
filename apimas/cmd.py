a04c63aa3bcd40a10dafd810cdc9c6e3e22ed8db
Thodoris Sotiropoulos <theosot@grnet.gr>
2017-02-21 15:44:23 +0200

    Allow user to specify the conf file to their own location
    A user had to specify a conf file file named `.apimas` under their home
    directory. However, now, a user is able to specify this conf file to
    their own location. They can provide this setting by defining the
    `--config`` option.
    
    Example:
    
    ```
    apimas --config myloc/conf foo-create
    ```
    If `--config` is not specified, apimas tries to find this file at
    `~/.apimas` or you can set an environment variable instead.

--- apimas/cmd.py
+++ apimas/cmd.py
@@ -1,18 +1,46 @@
-from apimas import utils
+import click
+from click import types
+from apimas import utils, exceptions as ex
 from apimas.cli.adapter import ApimasCliAdapter
 from apimas.clients.adapter import ApimasClientAdapter
 
 
-def main():
-    data = utils.load_config()
-    root_url = data['root']
-    spec = data['spec']
+def _construct_cli(root_url, spec):
     client_gen = ApimasClientAdapter(root_url)
     client_gen.construct(spec)
     cli = ApimasCliAdapter(client_gen.get_clients())
     cli.construct(spec)
-    base_command = cli.get_base_command()
-    base_command()
+    return cli
+
+
+class ApimasCLI(click.MultiCommand):
+
+    def get_command(self, ctx, name):
+        config_file = ctx.params.get('config')
+        try:
+            config = utils.configure(path=config_file)
+        except ex.ApimasException as e:
+            raise click.BadOptionUsage(str(e))
+        root_url, spec = config['root'], config['spec']
+        cli = _construct_cli(root_url, spec)
+        if name is None:
+            return cli.get_base_command()
+        return cli.endpoint_groups.get(name)
+
+
+@click.group(cls=ApimasCLI, invoke_without_command=True)
+@click.option('--config', type=types.Path(exists=False),
+              envvar='APIMAS_CONFIG')
+@click.pass_context
+def apimas(ctx, config):
+    os_args = click.get_os_args()
+    if not os_args or os_args[-1] == config:
+        cmd = ctx.command.get_command(ctx, None)
+        click.echo(cmd.get_help(ctx))
+
+
+def main():
+    apimas()
 
 
 if __name__ == '__main__':
