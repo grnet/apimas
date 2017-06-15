1634006865d77744880100ed62375816a7a48f9f
Thodoris Sotiropoulos <theosot@grnet.gr>
2017-06-15 16:37:29 +0300

    Create closures for custom validators (CerberusValidation)

--- apimas/apimas/components/processors.py
+++ apimas/apimas/components/processors.py
@@ -3,12 +3,8 @@ from collections import Iterable, Mapping
 from apimas import documents as doc, serializers as srs, utils
 from apimas.components import BaseProcessor
 from apimas.errors import InvalidSpec, ValidationError
-from apimas.constructors import Flag, Object
+from apimas.constructors import Flag, Object, Dummy
 from apimas.validators import CerberusValidator
-
-
-def default(context):
-    return context.instance
 
 
 class BaseSerialization(BaseProcessor):
@@ -56,12 +52,12 @@ class BaseSerialization(BaseProcessor):
                              last=True),
         'readonly':   Flag('readonly'),
         'writeonly':  Flag('writeonly'),
-        'default':    default,
+        'default':    Dummy(),
     }
 
-    def __init__(self, spec):
-        spec = deepcopy(spec)
-        self.spec = spec.get('*')
+    def __init__(self, collection, spec):
+        super(BaseSerialization, self).__init__(collection, spec)
+        self.spec = self.spec.get('*')
         if self.spec is None:
             msg = 'Processor {!r}: Node \'*\' of given spec is empty'
             raise InvalidSpec(msg.format(self.name))
@@ -140,43 +136,6 @@ class Serialization(BaseSerialization):
         return {'data': serializer.serialize(data)}
 
 
-def _validator_constructor(context):
-    """
-    Constuctor for `.validator` predicate.
-
-    This constructor is provided by the
-    `apimas.components.processors.CerberusValidation` processor for the
-    invocation of custom validators.
-
-    These validators can be performed on field level or on top level
-    (i.e. validating all data).
-
-    The signature of custom validators should be the following:
-        - (loc, value)
-
-    where `loc` is the location of the node being validated at spec and
-    `value` is the runtime value of it based on the request data.
-
-    If validation failed, methods should raise `apimas.errors.ValidationError`.
-    """
-
-    def to_cerberus(func):
-        def cerberus_validator_wrapper(field, value, error):
-            try:
-                func(context.loc[:-1], value)
-            except ValidationError as e:
-                error(field, e.message)
-        return cerberus_validator_wrapper
-
-    funcs = context.spec
-    if not isinstance(funcs, list):
-        msg = 'A list of callables is expected, not {!r}'
-        raise InvalidSpec(msg.format(type(funcs)), loc=context.loc)
-    cerberus_funcs = [to_cerberus(utils.import_object(func)) for func in funcs]
-    context.instance.update({'validator': cerberus_funcs})
-    return context.instance
-
-
 class CerberusValidation(BaseProcessor):
     """
     Processor for validating request data using Cerberus
@@ -184,7 +143,7 @@ class CerberusValidation(BaseProcessor):
     """
     name = 'apimas.components.processors.CerberusValidation'
 
-    CONSTRUCTORS = {
+    constructors = {
         'ref':        Object(dict, kwargs_instance=True,
                              extra={'type': 'string'}),
         'integer':    Object(dict, kwargs_instance=True,
@@ -204,11 +163,9 @@ class CerberusValidation(BaseProcessor):
         'datetime':   Object(dict, kwargs_instance=True,
                              extra={'type': 'datetime'}),
         'date':       Object(dict, kwargs_instance=True, kwargs_spec=True,
-                             extra={'type': 'string'},
-                             kwargs_spec_mapping={'date': 'date_format'}),
+                             extra={'type': 'date'}),
         'datetime':   Object(dict, kwargs_instance=True, kwargs_spec=True,
-                             extra={'type': 'string'},
-                             kwargs_spec_mapping={'date': 'date_format'}),
+                             extra={'type': 'datetime'}),
         'file':       Object(dict, kwargs_instance=True, kwargs_spec=True),
         'struct':     Object(dict, kwargs_instance=True, args_spec=True,
                              args_spec_name='schema', extra={'type': 'dict'}),
@@ -217,17 +174,17 @@ class CerberusValidation(BaseProcessor):
         'readonly':   Flag('readonly'),
         'required':   Flag('required'),
         'nullable':   Flag('nullable'),
-        'validator':  _validator_constructor,
-        'default':    default
+        'default':    Dummy(),
     }
 
-    READ_KEYS = {
-        'data': 'request/content',
-    }
+    READ_KEYS = DeSerialization.WRITE_KEYS
 
-    def __init__(self, spec):
-        spec = deepcopy(spec)
-        self.spec = spec.get('*')
+    def __init__(self, collection, spec):
+        super(CerberusValidation, self).__init__(collection, spec)
+
+        # Attach constructor for '.validator' predicate.
+        self.constructors.update({'validator': self._validator_constructor})
+        self.spec = self.spec.get('*')
         if self.spec is None:
             msg = 'Processor {!r}: Node \'*\' of given spec is empty'
             raise InvalidSpec(msg.format(self.name))
@@ -246,12 +203,52 @@ class CerberusValidation(BaseProcessor):
         }
 
     def _construct(self):
-        spec = self.spec
         instance = doc.doc_construct(
-            {}, spec, constructors=self.CONSTRUCTORS,
+            {}, self.spec, constructors=self.constructors,
             allow_constructor_input=False, autoconstruct='default',
             construct_spec=True)
         return instance
+
+    def _validator_constructor(self, context):
+        """
+        Constuctor for `.validator` predicate.
+
+        This constructor is provided by the
+        `apimas.components.processors.CerberusValidation` processor for the
+        invocation of custom validators.
+
+        These validators can be performed on field level or on top level
+        (i.e. validating all data).
+
+        The signature of custom validators should be the following:
+            - (loc, value)
+
+        where `loc` is the location of the node being validated at spec and
+        `value` is the runtime value of it based on the request data.
+
+        If validation failed, methods should raise `apimas.errors.ValidationError`.
+        """
+        field_location = self.collection + ('*',) + context.loc[:-1]
+        while field_location[-1].startswith('.'):
+            # Strip predicates from current location.
+            field_location = field_location[:-1]
+
+        def to_cerberus(func):
+            def cerberus_validator_wrapper(field, value, error):
+                try:
+                    func(field_location, value)
+                except ValidationError as e:
+                    error(field, e.message)
+            return cerberus_validator_wrapper
+
+        funcs = context.spec
+        if not isinstance(funcs, list):
+            msg = 'A list of callables is expected, not {!r}'
+            raise InvalidSpec(msg.format(type(funcs)), loc=context.loc)
+        cerberus_funcs = [to_cerberus(utils.import_object(func))
+                          for func in funcs]
+        context.instance.update({'validator': cerberus_funcs})
+        return context.instance
 
     def process(self, collection, url, action, context):
         """
