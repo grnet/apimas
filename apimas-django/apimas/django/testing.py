a330855e8fbb46897838ed4e360bbd8c73577ad7
Thodoris Sotiropoulos <theosot@grnet.gr>
2017-06-15 16:17:20 +0300

    testing: Refactor default methods for test cases
    Refactor default methods used for the default test cases in order to be
    function-based.

--- apimas-django/apimas/django/testing.py
+++ apimas-django/apimas/django/testing.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from copy import deepcopy
 import json
 import random
@@ -6,6 +7,7 @@ from django.test import TestCase as DjangoTestCase
 from django.test.client import MULTIPART_CONTENT
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from apimas import documents as doc
+from apimas.errors import InvalidInput
 from apimas.django import model_utils as mutils
 from apimas.utils import utils
 from apimas.django.generators import DjangoRequestGenerator
@@ -18,9 +20,8 @@ ARRAY_OF = '.array of='
 def _ref(spec, field_spec):
     refs = []
     ref = field_spec[REF]['to']
-    endpoint, collection = tuple(ref.split('/'))
     refs.append(ref)
-    refs.extend(get_ref_collections(spec, endpoint, collection))
+    refs.extend(get_ref_collections(spec, ref))
     return refs
 
 
@@ -45,172 +46,225 @@ def _get_refs(field_schema, spec):
     return refs
 
 
-def get_ref_collections(spec, endpoint, collection):
+def get_ref_collections(spec, collection):
+    endpoint, _, collection = collection.partition('/')
     loc = (endpoint, collection, '*')
     field_schema = {k: v for k, v in doc.doc_get(spec, loc).iteritems()
                     if not k.startswith('.')}
     return _get_refs(field_schema, spec)
 
 
-class TestCase(DjangoTestCase):
+def get_url(context, instances):
+    """
+    Construct URL to make a call to a collection or a resource of
+    a collection.
 
-    EXPECTED_STATUS_CODES = [
+    Args:
+        context (apimas.django.testing.TestingContext): Context
+        instances (dict): A dictionary of lists which contains all created
+            model instances per collection.
+
+    Returns:
+        str: URL which matches to the specified collection and action.
+    """
+    slash = '/'
+    iscollection = context.iscollection
+    action_url = context.action_url
+    if not iscollection and not instances:
+        raise InvalidInput('There is not any instance for {!r}'.format(
+            context.collection))
+    url = slash.join(['', context.collection, ''])
+    pattern = '' if iscollection\
+        else str(random.choice(instances).pk) + slash
+    url = urljoin(url, pattern)
+    if action_url and action_url != slash:
+        url = urljoin(url, action_url.strip(slash) + slash)
+    return url
+
+
+def get_content_and_type(context, instances):
+    """
+    Creates random data and specifies content type accordingly.
+
+    Args:
+        context (apimas.django.testing.TestingContext): Context
+        instances (dict): A dictionary of lists which contains all created
+            model instances per collection.
+    """
+    # Create random data based on the spec.
+    gen = DjangoRequestGenerator(
+        doc.doc_get(context.spec, tuple(context.collection.split('/'))),
+        instances=instances)
+    data = gen.construct()
+    if any(isinstance(v, (file, InMemoryUploadedFile))
+           for v in data.itervalues()):
+        return data, MULTIPART_CONTENT
+    # It is not supported to create body for a GET request
+    # by django client.
+    if context.action_method not in ['get', 'GET']:
+        data = json.dumps(data)
+    return data, 'application/json'
+
+
+def create_collection_instances(collection, spec, models,
+                                excluded_models=None):
+    """
+    Create an instance of the model associated with a collection.
+
+    Models along with their dependencies are extracted, and then a
+    topological sort algorithm is applied on the derived graph in order
+    required dependencies to be created with the right order..
+    """
+    collections = [collection] + get_ref_collections(spec, collection)
+    models = {k: v for k, v in models.iteritems()
+              if k in collections}
+    schema = mutils.get_models_to_create(models.values())
+    top_ordered_models = utils.topological_sort(schema)
+    instances = {}
+    collection_instances = {collection: []
+                            for collection in models.iterkeys()}
+    excluded = excluded_models or []
+    for model in top_ordered_models:
+        if model in excluded:
+            continue
+        instance = mutils.populate_random_model(model, instances=instances)
+        instances[model] = instance
+        for collection, collection_model in models.iteritems():
+            if model is collection_model:
+                collection_instances[collection].append(instance)
+    return collection_instances
+
+
+def setUp_default(context):
+    """
+    Setup a test scenario for a particular endpoint, collection and action.
+
+    A model instance corresponding to the model associated with the
+    provided collection (which belongs to a particular endpoint), is
+    created along with its dependencies.
+
+    In case of the `create` action, the instances of the dependencies
+    are created only.
+
+    Args:
+        context (apimas.django.testing.TestingContext): Context
+
+    Returns:
+        dict: A dictonary of lists which contains the created model instances
+            per collection.
+    """
+    if context.action == 'create':
+        # We do not need to create an instance corresponding to the
+        # specified collection, but only instances of the dependencies.
+        return create_collection_instances(
+            context.collection, context.spec, context.models,
+            excluded_models=[context.models.get(context.collection)])
+    else:
+        return create_collection_instances(
+            context.collection, context.spec, context.models)
+
+
+def prepare_request_default(context, instances):
+    """
+    Prepares a django request constructing all required arguments
+    for the invocation of the client.
+
+    Client needs:
+        * A URL to make its call.
+        * Any valid arguments for the invocation, such as data or
+         content type.
+
+    This method constructs randomly both the url and the arguments.
+
+    Args:
+        context (apimas.django.testing.TestingContext): Context
+        instances (dict): A dictionary of lists which contains all created
+            model instances per collection.
+
+    Returns:
+        str: A URL for the call.
+        dict: Keyword arguments for the invocation of the django test client.
+    """
+    test_url = get_url(context, instances.get(context.collection))
+    data, content_type = get_content_and_type(context, instances)
+    request_kwargs = {
+        'data': data,
+        'content_type': content_type,
+    }
+    return test_url, request_kwargs
+
+
+def validate_response_default(test_obj, context, instances, response):
+    """
+    Validates the response of the server.
+
+    This method tests that the status code of the response is not an
+    erroneus one.
+
+    Args:
+        test_obj: Instance of TestCase class.
+        context (apimas.django.testing.TestingContext): Context
+        instances (dict): A dictionary of lists which contains all created
+            model instances per collection.
+        response: Response of django test client.
+    """
+    expected_status_codes = [
         200,
         201,
-        204
+        204,
     ]
+    test_obj.assertTrue(response.status_code in expected_status_codes)
+
+
+TESTING_CONTEXT_PARAMS = [
+    'collection',
+    'action',
+    'iscollection',
+    'action_method',
+    'action_url',
+    'adapter',
+    'models',
+    'spec',
+]
+TestingContext = namedtuple('TestingContext', TESTING_CONTEXT_PARAMS)
+
+
+class TestCase(DjangoTestCase):
 
     def setUp(self):
         self.test_url = None
         self.request_kwargs = {}
         self.models = self.adapter.models
-        self.spec = deepcopy(self.spec)
-
-    def create_instances(self, endpoint, collection, excluded_models=None):
-        """
-        Create instance of the model associated with the collection model
-        along with its dependencies.
-
-        Models along with their dependencies are specifies, and then a
-        topological sort algorithm is applied on the derived graph in order
-        model to be created with the right sequence.
-        """
-        collections = [endpoint + '/' + collection] + get_ref_collections(
-            self.spec, endpoint, collection)
-        models = {k: v for k, v in self.models.iteritems()
-                  if k in collections}
-        schema = mutils.get_models_to_create(models.values())
-        top_ordered_models = utils.topological_sort(schema)
-        instances = {}
-        collection_instances = {collection: []
-                                for collection in models.iterkeys()}
-        excluded = excluded_models or []
-        for model in top_ordered_models:
-            if model in excluded:
-                continue
-            instance = mutils.populate_random_model(model, instances=instances)
-            instances[model] = instance
-            for collection, collection_model in models.iteritems():
-                if model is collection_model:
-                    collection_instances[collection].append(instance)
-        return collection_instances
-
-    def setUp_default(self, endpoint, collection, action, action_spec):
-        """
-        Setup a test scenario for a particular endpoint, collection and action.
-
-        A model instance corresponding to the model associated with the
-        provided collection (which belongs to a particular endpoint), is
-        created along with its dependencies.
-
-        In case of the `create` action, the instances of the dependencies
-        are created only.
-
-        Args:
-            endpoint (str): Endpoint to which collection belongs.
-            collection (str): Name of inspected collection.
-            action (str): Name of action that is currenly tested.
-            action_spec (dict): Dictionary acting as a descriptor of the
-                action (e.g. http method, action url, subject).
-        """
-        if action == 'create':
-            # We do not need to create an instance corresponding to the
-            # specified collection, but only instances of the dependencies.
-            full_collection_name = endpoint + '/' + collection
-            self.collection_instances = self.create_instances(
-                endpoint, collection,
-                excluded_models=[self.models.get(full_collection_name)])
-        else:
-            self.collection_instances = self.create_instances(
-                endpoint, collection)
-
-    def _get_url(self, endpoint, collection, action_spec):
-        slash = '/'
-        iscollection = action_spec['iscollection']
-        action_url = action_spec['url']
-        instances = self.collection_instances.get(
-            endpoint + slash + collection)
-        url = slash.join(['', endpoint, collection, ''])
-        pattern = '' if iscollection\
-            else str(random.choice(instances).pk) + slash
-        url = urljoin(url, pattern)
-        if action_url and action_url != slash:
-            url = urljoin(url, action_url.strip(slash) + slash)
-        return url
-
-    def _get_content_and_type(self, endpoint, collection, action_spec):
-        # Create random data based on the spec.
-        gen = DjangoRequestGenerator(
-            doc.doc_get(self.spec, (endpoint, collection)),
-            instances=self.collection_instances)
-        data = gen.construct()
-        if any(isinstance(v, (file, InMemoryUploadedFile))
-               for v in data.itervalues()):
-            return data, MULTIPART_CONTENT
-        # It is not supported to create body for a GET request
-        # by django client.
-        if action_spec['method'] not in ['get', 'GET']:
-            data = json.dumps(data)
-        return data, 'application/json'
-
-    def prepare_request_default(self, endpoint, collection, action,
-                                action_spec):
-        """
-        Prepares a django request constructing all required arguments
-        for the invocation of the client.
-
-        Client needs:
-            * A URL to make its call (`self.test_url` field).
-            * Any valid arguments for the invocation, such as data or
-             content type (`self.request_kwargs`).
-
-        This method constructs randomly both the url and the arguments.
-
-        Args:
-            endpoint (str): Endpoint to which collection belongs.
-            collection (str): Name of inspected collection.
-            action (str): Name of action that is currenly tested.
-            action_spec (dict): Dictionary acting as a descriptor of the
-                action (e.g. http method, action url, subject).
-        """
-        self.test_url = self._get_url(
-            endpoint, collection, action_spec)
-        data, content_type = self._get_content_and_type(
-            endpoint, collection, action_spec)
-        self.request_kwargs.update({
-            'data': data,
-            'content_type': content_type,
-        })
-
-    def validate_response_default(self, endpoint, collection, action,
-                                  action_spec, response):
-        """
-        Validates the response of the server.
-
-        This method tests that the status code of the response is not an
-        erroneus one.
-
-        Args:
-            endpoint (str): Endpoint to which collection belongs.
-            collection (str): Name of inspected collection.
-            action (str): Name of action that is currenly tested.
-            action_spec (dict): Dictionary acting as a descriptor of the
-                action (e.g. http method, action url, subject).
-            response: Response object made by django client.
-        """
-        self.assertTrue(response.status_code in self.EXPECTED_STATUS_CODES)
+        self.context = {
+            'spec': deepcopy(self.spec),
+            'models': self.adapter.models,
+            'adapter': self.adapter,
+        }
 
     def _get_stage_method(self, property_name, dict_key):
         default_methods = {
-            'SETUP': self.setUp_default,
-            'REQUEST': self.prepare_request_default,
-            'VALIDATE': self.validate_response_default,
+            'SETUP': setUp_default,
+            'REQUEST': prepare_request_default,
+            'VALIDATE': validate_response_default,
         }
         methods_dict = getattr(self, property_name, None)
         default = default_methods[property_name]
         if methods_dict is None:
             return default
         return methods_dict.get(dict_key, default)
+
+    def _create_testing_context(self, endpoint, collection, action,
+                                action_spec):
+        return TestingContext(
+            collection=endpoint + '/' + collection,
+            action=action,
+            iscollection=action_spec['iscollection'],
+            action_url=action_spec['url'],
+            action_method=action_spec['method'],
+            spec=self.spec,
+            adapter=self.adapter,
+            models=self.models,
+        )
 
     def _template_test_case(self, endpoint, collection, action, action_spec):
         """
@@ -220,15 +274,17 @@ class TestCase(DjangoTestCase):
         This scenario includes the `setup`, `request preparation` and
         `response validation` stages.
         """
+        context = self._create_testing_context(
+            endpoint, collection, action, action_spec)
         key = (endpoint, collection, action)
         setup_method = self._get_stage_method('SETUP', key)
-        setup_method(endpoint, collection, action, action_spec)
+        instances = setup_method(context)
 
         prepare_method = self._get_stage_method('REQUEST', key)
-        prepare_method(endpoint, collection, action, action_spec)
+        test_url, kwargs = prepare_method(context, instances)
 
         client_method = getattr(self.client, action_spec['method'].lower())
-        response = client_method(self.test_url, **self.request_kwargs)
+        response = client_method(test_url, **kwargs)
 
         validate_method = self._get_stage_method('VALIDATE', key)
-        validate_method(endpoint, collection, action, action_spec, response)
+        validate_method(self, context, instances, response)
