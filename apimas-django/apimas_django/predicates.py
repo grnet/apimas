46385a53186e93260a5ec522c667d96659eaf4e2
Giorgos Korfiatis <gkorf@grnet.gr>
2018-01-11 13:01:13 +0200

    Write predicate specs more concisely

--- apimas-django/apimas_django/predicates.py
+++ apimas-django/apimas_django/predicates.py
@@ -1,175 +1,155 @@
 import docular
 
-PREDICATES = {}
-def register_predicate(predicate, spec):
-    docular.doc_spec_register_predicate(PREDICATES, predicate, spec)
+spec_list = [
+    {'.boolean': {}},
+    {'.string': {}},
 
+    {'.handler': {}, '.string': {}},
 
-def register_base_predicate(predicate):
-    register_predicate(predicate, {predicate: {}})
+    {'.flag.*': {}},
+    {'.flag.readonly': {}},
+    {'.flag.nullable': {}},
+    {'.flag.filterable': {}},
 
+    {'.fields': {}},
 
-register_base_predicate('.boolean')
-register_base_predicate('.string')
+    {
+        '.field.*': {},
+        'source': {'.string': {}},
+        '.flag.*': {},
+        '*': {},
+    },
 
-handler_spec = {'.handler': {}, '.string': {}}
-register_predicate('.handler', handler_spec)
+    {'.field.string': {}},
+    {'.field.serial': {}},
+    {'.field.integer': {}},
+    {'.field.identity': {'to': {'.string': {}}}},
 
-register_base_predicate('.flag.*')
-register_base_predicate('.flag.readonly')
-register_base_predicate('.flag.nullable')
-register_base_predicate('.flag.filterable')
+    {'.meta': {}},
 
+    {
+        '.field.collection.*': {},
+        'actions': {
+            '.action-template.*': {},
+            '*': {
+                ".action": {},
+                'on_collection': {'.boolean': {}},
+                'method': {'.string': {}},
+                'url': {'.string': {}},
+                'handler': {".handler": {}},
+                'pre': {"*": {".handler": {}}},
+                'post': {"*": {".handler": {}}},
+            }
+        },
+        'fields': {'*': {".field.*": {}}},
+        '*': {},
+    },
 
-register_base_predicate('.fields')
-
-field_spec = {'.field.*': {},
-              'source': {'.string': {}},
-              '.flag.*': {},
-              '*': {},
-}
-register_predicate('.field.*', field_spec)
-
-register_base_predicate('.field.string')
-register_base_predicate('.field.serial')
-register_base_predicate('.field.integer')
-
-field_identity_spec = {'.field.identity': {'to': {'.string': {}}}}
-register_predicate('.field.identity', field_identity_spec)
-
-register_base_predicate('.meta')
-
-field_collection_spec = {
-    '.field.collection.*': {},
-    'actions': {
-        '.action-template.*': {},
+    {
+        '.apimas_app': {},
+        '.meta': {},
         '*': {
-            ".action": {},
-            'on_collection': {'.boolean': {}},
-            'method': {'.string': {}},
-            'url': {'.string': {}},
-            'handler': {".handler": {}},
-            'pre': {"*": {".handler": {}}},
-            'post': {"*": {".handler": {}}},
-        }
-    },
-    'fields': {'*': {".field.*": {}}},
-    '*': {},
-}
-register_predicate('.field.collection.*', field_collection_spec)
-
-apimas_app_spec = {
-    '.apimas_app': {},
-    '.meta': {},
-    '*': {
-        ".endpoint": {},
-        ".meta": {},
-        "*": {'.field.collection.*': {}},
-    },
-}
-register_predicate('.apimas_app', apimas_app_spec)
-
-field_collection_django_spec = {
-    '.field.collection.django': {},
-    'model': {'.string': {}},
-    'bound': {'.string': {}},
-}
-register_predicate('.field.collection.django', field_collection_django_spec)
-
-action_template_django_create_spec = {
-    '.action-template.django.create': {},
-    'create': {
-        'method': 'POST',
-        'on_collection': True,
-        'url': '/',
-        'handler': 'apimas_django.handlers.CreateHandler',
-        'pre': {
-#            '1': 'apimas.components.processors.Authentication',
-#            '2': 'apimas_django.processors.UserRetrieval',
-            '3': 'apimas_django.processors.Permissions',
-            '4': 'apimas.components.processors.DeSerialization',
-            '5': 'apimas.components.processors.CerberusValidation',
-        },
-        'post': {
-            '1': 'apimas_django.processors.InstanceToDict',
-            '2': 'apimas.components.processors.Serialization'
+            ".endpoint": {},
+            ".meta": {},
+            "*": {'.field.collection.*': {}},
         },
     },
-}
-register_predicate('.action-template.django.create',
-                   action_template_django_create_spec)
 
-
-action_template_django_list_spec = {
-    '.action-template.django.list': {},
-    'list': {
-        'method': 'GET',
-        'on_collection': True,
-        'url': '/',
-        'pre': {
-#            '1': 'apimas.components.processors.Authentication',
-#            '2': 'apimas_django.processors.UserRetrieval',
-            '3': 'apimas_django.processors.Permissions',
-        },
-        'handler': 'apimas_django.handlers.ListHandler',
-        'post': {
-            '1': 'apimas_django.processors.Filtering',
-            '2': 'apimas_django.processors.InstanceToDict',
-            '3': 'apimas.components.processors.Serialization',
-        }
+    {
+        '.field.collection.django': {},
+        'model': {'.string': {}},
+        'bound': {'.string': {}},
     },
-}
-register_predicate('.action-template.django.list',
-                   action_template_django_list_spec)
 
-
-action_template_django_retrieve_spec = {
-    '.action-template.django.retrieve': {},
-    'retrieve': {
-        'method': 'GET',
-        'on_collection': False,
-        'url': '/',
-        'pre': {
-#            '1': 'apimas.components.processors.Authentication',
-#            '2': 'apimas_django.processors.UserRetrieval',
-#            '3': 'apimas_django.processors.ObjectRetrieval',
-            '4': 'apimas_django.processors.Permissions',
-        },
-        'handler': 'apimas_django.handlers.RetrieveHandler',
-        'post': {
-            '1': 'apimas_django.processors.InstanceToDict',
-            '2': 'apimas.components.processors.Serialization',
-        }
-    },
-}
-
-register_predicate('.action-template.django.retrieve',
-                   action_template_django_retrieve_spec)
-
-action_template_django_partial_update_spec = {
-    '.action-template.django.partial_update': {},
-    'partial_update': {
-        'method': 'PATCH',
-        'on_collection': False,
-        'url': '/',
-        'pre': {
-#                'apimas.components.processors.Authentication',
-#                'apimas_django.processors.UserRetrieval',
-#                'apimas_django.processors.ObjectRetrieval',
-            '4': 'apimas_django.processors.Permissions',
-            '5': 'apimas.components.processors.DeSerialization',
-            '6': 'apimas.components.processors.CerberusValidation',
-        },
-        'handler': 'apimas_django.handlers.UpdateHandler',
-        'post': {
-            '1': 'apimas_django.processors.InstanceToDict',
-            '2': 'apimas.components.processors.Serialization',
+    {
+        '.action-template.django.create': {},
+        'create': {
+            'method': 'POST',
+            'on_collection': True,
+            'url': '/',
+            'handler': 'apimas_django.handlers.CreateHandler',
+            'pre': {
+    #            '1': 'apimas.components.processors.Authentication',
+    #            '2': 'apimas_django.processors.UserRetrieval',
+                '3': 'apimas_django.processors.Permissions',
+                '4': 'apimas.components.processors.DeSerialization',
+                '5': 'apimas.components.processors.CerberusValidation',
+            },
+            'post': {
+                '1': 'apimas_django.processors.InstanceToDict',
+                '2': 'apimas.components.processors.Serialization'
+            },
         },
     },
-}
 
-register_predicate('.action-template.django.partial_update',
-                   action_template_django_partial_update_spec)
+    {
+        '.action-template.django.list': {},
+        'list': {
+            'method': 'GET',
+            'on_collection': True,
+            'url': '/',
+            'pre': {
+    #            '1': 'apimas.components.processors.Authentication',
+    #            '2': 'apimas_django.processors.UserRetrieval',
+                '3': 'apimas_django.processors.Permissions',
+            },
+            'handler': 'apimas_django.handlers.ListHandler',
+            'post': {
+                '1': 'apimas_django.processors.Filtering',
+                '2': 'apimas_django.processors.InstanceToDict',
+                '3': 'apimas.components.processors.Serialization',
+            }
+        },
+    },
+
+    {
+        '.action-template.django.retrieve': {},
+        'retrieve': {
+            'method': 'GET',
+            'on_collection': False,
+            'url': '/',
+            'pre': {
+    #            '1': 'apimas.components.processors.Authentication',
+    #            '2': 'apimas_django.processors.UserRetrieval',
+    #            '3': 'apimas_django.processors.ObjectRetrieval',
+                '4': 'apimas_django.processors.Permissions',
+            },
+            'handler': 'apimas_django.handlers.RetrieveHandler',
+            'post': {
+                '1': 'apimas_django.processors.InstanceToDict',
+                '2': 'apimas.components.processors.Serialization',
+            }
+        },
+    },
+
+    {
+        '.action-template.django.partial_update': {},
+        'partial_update': {
+            'method': 'PATCH',
+            'on_collection': False,
+            'url': '/',
+            'pre': {
+    #                'apimas.components.processors.Authentication',
+    #                'apimas_django.processors.UserRetrieval',
+    #                'apimas_django.processors.ObjectRetrieval',
+                '4': 'apimas_django.processors.Permissions',
+                '5': 'apimas.components.processors.DeSerialization',
+                '6': 'apimas.components.processors.CerberusValidation',
+            },
+            'handler': 'apimas_django.handlers.UpdateHandler',
+            'post': {
+                '1': 'apimas_django.processors.InstanceToDict',
+                '2': 'apimas.components.processors.Serialization',
+            },
+        },
+    },
+]
+
+PREDICATES = {}
+
+for spec in spec_list:
+    docular.doc_scan_spec(spec, PREDICATES)
+
 
 # PREDICATES =  {
 #     '.action-template.django.update': {
