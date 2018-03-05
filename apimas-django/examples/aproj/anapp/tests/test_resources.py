73fc70ced649f99f6595875b9e0165340a207904
Kostas Papadimitriou <kpap@grnet.gr>
2018-03-05 19:06:51 +0200

    tests: Update tests
    - Test authentication

--- apimas-django/examples/aproj/anapp/tests/test_resources.py
+++ apimas-django/examples/aproj/anapp/tests/test_resources.py
@@ -1,5 +1,20 @@
 from apimas_django.test import *
 
+pytestmark = pytest.mark.django_db(transaction=False)
+
+
+def test_posts(client):
+    api = client.copy(prefix='/api/prefix/')
+    admin = client.copy(prefix='/api/prefix', auth_token='admin-admin-1234')
+    assert api.get('posts').json() == []
+
+    post = dict(title="Post title", body="Post content")
+    resp = api.post('posts', post)
+    assert resp.status_code == 403
+
+    resp = admin.post('posts', post)
+    assert resp.status_code == 201
+
 
 def test_groups(client):
     """
@@ -8,13 +23,44 @@ def test_groups(client):
     api = client.copy(prefix='/api/prefix/')
     assert api.get('groups').json() == []
 
-    group1 = api.post('groups', {'name': 'users'}).json()
-    assert group1.get('name') == 'users'
-    assert group1.get('id') == 1
-    group2 = api.post('groups', {'name': 'users'}).json()
+    resp = api.post('groups', {
+        'name': 'users',
+        'founded': '2014-12-31',
+        'active': True
+    })
+    assert resp.status_code == 400
+    assert "'email': Field is required" in resp.json().get("details")
+
+    resp = api.post('groups', {
+        'name': 'users',
+        'founded': '2014-12-31',
+        'active': True,
+        'email': 'users@apim.as'
+    })
+    assert resp.status_code == 201
+
+    group1 = resp.json()
+    # test create_response permissions
+    assert u'name' in group1
+    assert u'founded' in group1
+    assert u'active' in group1
+
+    resp = api.post('groups', {
+        'name': 'users',
+        'founded': '2014-12-31',
+        'email': 'group@api.mas',
+        'active': True
+    })
+    assert resp.status_code == 201
+
+    assert group1.get('url').endswith('groups/{}/'.format(group1.get('id')))
+    group1 = api.get(group1.get('url'))
+    assert group1.status_code == 200
 
     # groups created
-    assert len(api.get('groups').json()) == 2
+    resp = api.get('groups')
+    assert resp.status_code == 200
+    assert len(resp.json()) == 2
 
     # method not allowed
     assert api.delete('groups/1').status_code == 405
