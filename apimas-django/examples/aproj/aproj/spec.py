268149e3226f295502788c4fbb04d6edf7284040
Giorgos Korfiatis <gkorf@grnet.gr>
2018-03-21 14:52:59 +0200

    Split and cleanup example test

--- apimas-django/examples/aproj/aproj/spec.py
+++ apimas-django/examples/aproj/aproj/spec.py
@@ -2,203 +2,160 @@ DEPLOY_CONFIG = {
     ":root_url": "http://127.0.0.1:8000/",
 }
 
+POSTS = {
+    ".field.collection.django": {},
+
+    ":authenticator": "apimas.auth.TokenAuthentication",
+    ":verifier": "anapp.auth.token_verifier",
+    ":user_resolver": "anapp.auth.user_resolver",
+
+    "model": "anapp.models.Post",
+    "actions": {
+        '.action-template.django.list': {},
+        '.action-template.django.create': {},
+        '.action-template.django.retrieve': {},
+    },
+    "fields": {
+        "id": {".field.serial": {}},
+        "url": {".field.identity": {},
+                '.flag.readonly': True,
+                "to": "api/prefix/groups",
+                "source": "id"},
+        "title": {".field.string": {}},
+        "body": {".field.string": {}}
+    }
+}
+
+NULLTEST = {
+    '.field.collection.django': {},
+    'model': 'anapp.models.Nulltest',
+    "actions": {
+        '.action-template.django.list': {},
+        '.action-template.django.create': {},
+        '.action-template.django.retrieve': {},
+    },
+    'fields': {
+        "id": {".field.serial": {}},
+        'fdef': {'.field.integer': {},
+                 '.flag.nullable.default': {}},
+        'fnodef': {'.field.integer': {},
+                   '.flag.nullable': {}},
+    },
+}
+
+INSTITUTIONS = {
+    ".field.collection.django": {},
+    "model": "anapp.models.Institution",
+    "actions": {
+        '.action-template.django.list': {},
+        '.action-template.django.retrieve': {},
+        '.action-template.django.create': {},
+        '.action-template.django.partial_update': {},
+        '.action-template.django.update': {},
+        '.action-template.django.delete': {},
+    },
+    "fields": {
+        "id": {".field.serial": {}},
+        "name": {".field.string": {},
+                 '.flag.orderable': {}},
+        "active": {".field.boolean": {},
+                   "default": True,
+                   ".flag.orderable": {}},
+        "category": {".field.choices": {},
+                     "allowed": {
+                         '=': ["Institution", "Research"]},
+                     "displayed": {
+                     '=': ["Institution", "Research Center"]},
+                     "default": "Research"},
+        "category_raw": {
+            ".field.choices": {},
+            ".flag.readonly": {},
+            "source": "category",
+            "allowed": {
+                '=': ["Institution", "Research"]},
+        },
+        "logo": {'.field.file': {},
+                 'default': ''},
+    }
+}
+
+GROUPS = {
+    ".field.collection.django": {},
+    "model": "anapp.models.Group",
+    "actions": {
+        '.action-template.django.list': {},
+        '.action-template.django.partial_update': {},
+        '.action-template.django.create': {},
+        '.action-template.django.retrieve': {},
+        '.action-template.django.delete': {},
+    },
+    "fields": {
+        "id": {".field.serial": {}},
+        "url": {".field.identity": {},
+                '.flag.readonly': True,
+                "to": "api/prefix/groups",
+                "source": "id"},
+        "name": {".field.string": {}},
+        "founded": {".field.date": {}},
+        "active": {".field.boolean": {},
+                   '.flag.filterable': {},
+                   'default': True},
+        "email": {".field.email": {}},
+        "institution_id": {".field.ref": {},
+                           '.flag.filterable': {},
+                           "to": "api/prefix/institutions"},
+        "users": {
+            '.field.collection.django': {},
+            ':filter_compat': True,
+            'model': 'anapp.models.User',
+            'source': 'users',
+            'bound': 'group',
+            'actions': {
+                '.action-template.django.list': {},
+                '.action-template.django.create': {},
+                '.action-template.django.retrieve': {},
+                '.action-template.django.partial_update': {},
+                '.action-template.django.delete': {},
+            },
+            'fields': {
+                'id': {'.field.serial': {}},
+                'onoma': {'.field.string': {},
+                          '.flag.filterable': {},
+                          '.flag.searchable': {},
+                          '.flag.orderable': {},
+                          'source': 'username'},
+                "variants": {
+                    ".field.struct": {},
+                    '.flag.nullable': {},
+                    'default': {'=': None},
+                    "source": "name_variants",
+                    "fields": {
+                        "en": {".field.string": {},
+                               '.flag.searchable': {},
+                               '.flag.filterable': {},
+                               '.flag.orderable': {}},
+                        "el": {".field.string": {},
+                               '.flag.searchable': {}},
+                    },
+                },
+                'age': {'.field.integer': {},
+                        '.flag.filterable': {}},
+            }
+        }
+    }
+}
+
 APP_CONFIG = {
     ".apimas_app": {},
     ":permission_rules": "anapp.rules.get_rules",
     'endpoints': {
         "api/prefix": {
             "collections": {
-                "posts": {
-                    ".field.collection.django": {},
-
-                    ":authenticator": "apimas.auth.TokenAuthentication",
-                    ":verifier": "anapp.auth.token_verifier",
-                    ":user_resolver": "anapp.auth.user_resolver",
-
-                    "model": "anapp.models.Post",
-                    "actions": {
-                        '.action-template.django.list': {},
-                        '.action-template.django.create': {},
-                        '.action-template.django.retrieve': {},
-                    },
-                    "fields": {
-                        "id": {".field.serial": {}},
-                        "url": {".field.identity": {},
-                                '.flag.readonly': True,
-                                "to": "api/prefix/groups",
-                                "source": "id"},
-                        "title": {".field.string": {}},
-                        "body": {".field.string": {}}
-                    }
-                },
-                'nulltest': {
-                    '.field.collection.django': {},
-                    'model': 'anapp.models.Nulltest',
-                    "actions": {
-                        '.action-template.django.list': {},
-                        '.action-template.django.create': {},
-                        '.action-template.django.retrieve': {},
-                    },
-                    'fields': {
-                        "id": {".field.serial": {}},
-                        'fdef': {'.field.integer': {},
-                                 '.flag.nullable.default': {}},
-                        'fnodef': {'.field.integer': {},
-                                   '.flag.nullable': {}},
-                    },
-                },
-                "institutions": {
-                    ".field.collection.django": {},
-                    "model": "anapp.models.Institution",
-                    "actions": {
-                        '.action-template.django.list': {},
-                        '.action-template.django.retrieve': {},
-                        '.action-template.django.create': {},
-                        '.action-template.django.partial_update': {},
-                        '.action-template.django.update': {},
-                        '.action-template.django.delete': {},
-                    },
-                    "fields": {
-                        "id": {".field.serial": {}},
-                        "name": {".field.string": {},
-                                 '.flag.orderable': {}},
-                        "active": {".field.boolean": {},
-                                   "default": True,
-                                   ".flag.orderable": {}},
-                        "category": {".field.choices": {},
-                                     "allowed": {
-                                         '=': ["Institution", "Research"]},
-                                     "displayed": {
-                                     '=': ["Institution", "Research Center"]},
-                                     "default": "Research"},
-                        "category_raw": {
-                            ".field.choices": {},
-                            ".flag.readonly": {},
-                            "source": "category",
-                            "allowed": {
-                                '=': ["Institution", "Research"]},
-                        },
-                        "logo": {'.field.file': {},
-                                 'default': ''},
-                    }
-                },
-                "groups": {
-                    ".field.collection.django": {},
-                    "model": "anapp.models.Group",
-                    "actions": {
-                        '.action-template.django.list': {},
-                        '.action-template.django.partial_update': {},
-                        '.action-template.django.create': {},
-                        '.action-template.django.retrieve': {},
-                        '.action-template.django.delete': {},
-                    },
-                    "fields": {
-                        "id": {".field.serial": {}},
-                        "url": {".field.identity": {},
-                                '.flag.readonly': True,
-                                "to": "api/prefix/groups",
-                                "source": "id"},
-                        "name": {".field.string": {}},
-                        "founded": {".field.date": {}},
-                        "active": {".field.boolean": {},
-                                   '.flag.filterable': {},
-                                   'default': True},
-                        "email": {".field.email": {}},
-                        "institution_id": {".field.ref": {},
-                                           '.flag.filterable': {},
-                                           "to": "api/prefix/institutions"},
-                        "users": {
-                            '.field.collection.django': {},
-                            ':filter_compat': True,
-                            'model': 'anapp.models.User',
-                            'source': 'users',
-                            'bound': 'group',
-                            'actions': {
-                                '.action-template.django.list': {},
-                                '.action-template.django.create': {},
-                                '.action-template.django.retrieve': {},
-                                '.action-template.django.partial_update': {},
-                                '.action-template.django.delete': {},
-                            },
-                            'fields': {
-                                'id': {'.field.serial': {}},
-                                'onoma': {'.field.string': {},
-                                          '.flag.filterable': {},
-                                          '.flag.searchable': {},
-                                          '.flag.orderable': {},
-                                          'source': 'username'},
-                                "variants": {
-                                    ".field.struct": {},
-                                    '.flag.nullable': {},
-                                    'default': {'=': None},
-                                    "source": "name_variants",
-                                    "fields": {
-                                        "en": {".field.string": {},
-                                               '.flag.searchable': {},
-                                               '.flag.filterable': {},
-                                               '.flag.orderable': {}},
-                                        "el": {".field.string": {},
-                                               '.flag.searchable': {}},
-                                    },
-                                },
-                                'age': {'.field.integer': {},
-                                        '.flag.filterable': {}},
-
-                        #         'emails': {
-                        #             '.field.collection.django': {},
-                        #             'model': 'anapp.models.Email',
-                        #             'source': 'email_set',
-                        #             'bound': 'user',
-                        #             'actions': {
-                        #                 '.action-template.django.list': {},
-                        #                 '.action-template.django.create': {},
-                        #                 '.action-template.django.retrieve': {},
-                        #             },
-                        #             'fields': {
-                        #                 'id': {'.field.serial': {}},
-                        #                 'email': {'.field.string': {}},
-                        #             }
-                        #         }
-                            }
-                        }
-                    }
-                }
+                "posts": POSTS,
+                'nulltest': NULLTEST,
+                "institutions": INSTITUTIONS,
+                "groups":GROUPS,
             },
         },
     },
-    # "api/prefix2": {
-    #    ".endpoint": {},
-    #     "groups": {
-    #         ".field.collection.django": {},
-    #         "model": "anapp.models.Group",
-    #         "actions": {
-    #             '.action-template.django.list': {},
-    #             '.action-template.django.create': {},
-    #             '.action-template.django.retrieve': {},
-    #         },
-    #         "fields": {
-    #             "id": {".field.serial": {}},
-    #             "url": {".field.identity": {"to": "api/prefix2/groups"},
-    #                     "source": "id"},
-    #             "name": {".field.string": {}},
-    #         }
-    #     },
-    #     "users": {
-    #         '.field.collection.django': {},
-    #         '.field.collection.*': {},
-    #         'model': 'anapp.models.User',
-    #         'source': 'user_set',
-    #         'bound': 'group',
-    #         'actions': {
-    #             '.action-template.django.list': {},
-    #             '.action-template.django.create': {},
-    #             '.action-template.django.retrieve': {},
-    #         },
-    #         'fields': {
-    #             'id': {'.field.serial': {}},
-    #             'username': {'.field.string': {}},
-    #             'group': {'.field.ref': {"to": "api/prefix2/groups/fields"}},
-    #         }
-    #     },
-    # },
 }
