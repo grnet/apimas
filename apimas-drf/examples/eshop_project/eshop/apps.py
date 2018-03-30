e512ef3a5269b88366c3dd377fc57fa66eb89ec6
Giorgos Korfiatis <gkorf@grnet.gr>
2018-03-30 10:46:59 +0300

    Remove obsolete apimas-drf package

--- apimas-drf/examples/eshop_project/eshop/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EshopConfig(AppConfig):
-    name = 'eshop'
--- apimas-drf/examples/eshop_project/eshop/hooks.py
+++
@@ -1,8 +0,0 @@
-from apimas.drf import mixins
-from eshop.models import Order
-
-
-class CreateOrder(mixins.HookMixin):
-    def preprocess_create(self):
-        # Initial status
-        self.stash(extra={'status': Order.RECEIVED})
