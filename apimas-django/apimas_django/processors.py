3e5d4eb709f8772eb01d0f490389d0575eaea0d6
Giorgos Korfiatis <gkorf@grnet.gr>
2018-03-30 13:52:42 +0300

    Cleanup instance to dict processor

--- apimas-django/apimas_django/processors.py
+++ apimas-django/apimas_django/processors.py
@@ -1,18 +1,8 @@
-from django.core.exceptions import FieldDoesNotExist
 from django.db.models  import Model
 from django.db.models.query import QuerySet
-from apimas import documents as doc, utils
-from apimas.errors import (AccessDeniedError, ConflictError, InvalidInput,
-                           InvalidSpec)
+from apimas.errors import InvalidInput
 from apimas.components import BaseProcessor, ProcessorConstruction
-from apimas_django import utils as django_utils
-from apimas.converters import Date, DateTime, Integer, Float, Boolean, List
 import docular
-
-
-REF = '.ref'
-STRUCT = '.struct='
-ARRAY_OF = '.array of='
 
 
 def no_constructor(instance):
@@ -88,65 +78,7 @@ class InstanceToDictProcessor(BaseProcessor):
         self.on_collection = on_collection
         self.field_spec = fields
 
-    # def _extract_many(self, instance, field_name):
-    #     """
-    #     Extracts the value of a many to many or one to many django model
-    #     relation.
-    #     """
-    #     try:
-    #         return getattr(instance, field_name).all()
-    #     except AttributeError:
-    #         return getattr(instance, field_name + '_set').all()
-
-    # def _extract_rel_id(self, instance, field_name):
-    #     """
-    #     Extracts the id of a one to one or many to one django model
-    #     relation.
-    #     """
-    #     try:
-    #         return getattr(instance, field_name + '_id')
-    #     except AttributeError:
-    #         return getattr(instance, field_name)
-
-    # def _extract_rel(self, orm_model, instance, field, field_spec):
-    #     """
-    #     Helper function to get the python native format of a django
-    #     related field.
-    #     """
-    #     many = field.many_to_many or field.one_to_many
-    #     source = docular.doc_spec_get(field_spec['source']) or field.name
-    #     if many:
-    #         value = self._extract_many(instance, source)
-    #         if REF in field_spec[ARRAY_OF]:
-    #             return [getattr(v, 'pk') for v in value]
-    #         return [
-    #             self.to_dict(
-    #                 field.related_model, v,
-    #                 field_spec[ARRAY_OF][STRUCT]
-    #             ) for v in value
-    #         ]
-    #     if not hasattr(instance, field.name):
-    #         return None
-    #     if REF in field_spec:
-    #         return self._extract_rel_id(instance, field.name)
-    #     return self.to_dict(field.related_model, getattr(instance, source),
-    #                         field_spec['.struct='])
-
     def to_dict(self, instance, spec):
-        """
-        Constructs a given model instance a python dict.
-
-        Only the model attributes which are declared on specification are
-        included in the returned dictionary.
-
-        Args:
-            orm_model: Django model associated with the instance.
-            instance: Model instance to be converted into a python dict.
-            spec (dict): Specification of collection.
-
-        Returns:
-            dict: Dictionary format of a model instance.
-        """
         if instance is None:
             return None
 
@@ -169,30 +101,10 @@ class InstanceToDictProcessor(BaseProcessor):
                 elif fields_type == 'struct':
                     value = self.to_dict(value, fields)
 
-            # try:
-            #     field = orm_model._meta.get_field(source)
-            #     print k, field.related_model
-            #     if field.related_model is None:
-            #         value = getattr(instance, field.name)
-            #     else:
-            #         value = self._extract_rel(orm_model, instance, field,
-            #                                   v)
-            # except FieldDoesNotExist:
-            #     # If instance does not have any field with that name, then
-            #     # check if there is any property-like.
-            #     value = getattr(instance, source)
-            #     print "GOT VALUE", value, "from source", source
             data[k] = value
         return data
 
     def execute(self, processor_data):
-        """
-        A processor which is responsible for converting a
-        `django.db.models.Model` or a `django.db.models.query.QuerySet`
-        instance into a python dict.
-
-        This dict holds only the information specified by the spec.
-        """
         instance = processor_data['instance']
         if instance is None:
             return (None,)
