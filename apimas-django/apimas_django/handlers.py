2d841c70489e3d843c5ce58524a31e63fd1b549d
Giorgos Korfiatis <gkorf@grnet.gr>
2018-03-30 16:28:15 +0300

    Cleanup handlers

--- apimas-django/apimas_django/handlers.py
+++ apimas-django/apimas_django/handlers.py
@@ -1,9 +1,8 @@
-from django.db.models import Model, ProtectedError
-from django.db.models.query import QuerySet
+from django.db.models import ProtectedError
 from apimas import utils
 from apimas_django import utils as django_utils
 from apimas.components import BaseProcessor, ProcessorConstruction
-from apimas.errors import ValidationError, AccessDeniedError
+from apimas.errors import AccessDeniedError
 import docular
 
 
@@ -16,11 +15,6 @@ Nothing = type('Nothing', (), {'__repr__': lambda self: 'Nothing'})()
 
 def no_constructor(instance):
     pass
-
-
-def copy_constructor(instance):
-    docular.doc_spec_set(instance,
-                         dict(docular.doc_spec_iter(instance)))
 
 
 def get_bounds(loc, top_spec):
@@ -339,7 +333,6 @@ def do_update(spec, data, instance, precreated=None):
     if precreated:
         update_args.update(precreated)
 
-    model = spec['model']
     update_args.update(get_fields(spec['subfields'], data))
 
     print "UPDATE ARGS", update_args
@@ -521,72 +514,3 @@ class DeleteHandlerProcessor(RetrieveHandlerProcessor):
 
 
 DeleteHandler = _django_base_construction(DeleteHandlerProcessor)
-
-
-class TokenAuthHandler(BaseProcessor):
-    """
-    Add handler for generating JWT tokens to authenticated parties.
-    """
-    READ_KEYS = {
-        'headers': 'request/meta/headers',
-    }
-
-    def __init__(self, collection, spec, auth_method,
-                 **meta):
-        super(TokenAuthHandler, self).__init__(collection, spec, **meta)
-
-        secret_key_extractor = meta.get('secret_key')
-        if secret_key_extractor is None:
-            raise InvalidInput('Secret key is missing')
-        secret_key_extractor = utils.import_object(secret_key_extractor)
-        assert callable(secret_key_extractor), 'Secret key must be a callable'
-
-        self.auth_method = auth_method
-
-        user_processor = meta.get('user_processor')
-        if user_processor:
-            self.user_processor = utils.import_object(user_processor)
-            assert callable(self.user_processor), (
-                '"user_processor" must be a callable')
-
-        token_gen = meta.get('token_generator')
-        if token_gen is None:
-            raise InvalidInput('Token generator is required')
-        self.token_gen = utils.import_object(token_gen)
-        assert callable(self.token_gen), ('"token_gen" must be a callable')
-
-    def process(self, collection, url, action, context):
-        """
-        Client is authenticated based on a specific authentication method,
-        specified by `auth_method` and then generates a new JWT token.
-        """
-        context_data = self.read(context)
-        user = self.auth_method.authenticate(context_data['headers'])
-        if self.user_processor:
-            user = self.user_processor(user)
-        content = self.token_gen(user)
-        return {
-            'content': content,
-            'meta': {
-                'content_type': 'application/json',
-                'status_code': 200,
-            }
-        }
-
-    def handle_error(self, component, cmp_args, ex):
-        # This handler maps only `UnauthorizedError` exceptions to status
-        # codes.
-        status_code = 401 if isinstance(ex, UnauthorizedError) else 500
-        return {
-            'content': {
-                'details': ex.message,
-            },
-            'meta': {
-                'content_type': 'application/json',
-                'status_code': status_code,
-                'headers': {
-                    'WWW-Authenticate': getattr(
-                        self.auth_method, 'AUTH_HEADERS', None)
-                },
-            }
-        }
