0ca611ab226011e2ade3da75e782984b012d88aa
Giorgos Korfiatis <gkorf@grnet.gr>
2018-05-10 11:55:59 +0300

    Integrate docular in testing and packaging

--- configure-testing.sh
+++ configure-testing.sh
@@ -1,7 +1,6 @@
 cd "$(dirname "$0")"
 ROOT=$(pwd)
 
+cd docular && python setup.py install && cd ..
 cd apimas && python setup.py install && cd ..
 cd apimas-django && python setup.py install && pip install -r requirements_dev.txt && cd ..
-
-
--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
 pytest --nomigrations --pdb -s --disable-warnings --tb=no --maxfail=1 apimas-django
+pytest --nomigrations --pdb -s --disable-warnings --tb=no --maxfail=1 docular
